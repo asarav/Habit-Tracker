d8ab517673163a8a4c7f4d6eb222304368e6bc73
asarav <asarav@umich.edu>
2019-02-16 11:57:27 -0500

    Implement proper routintg for all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoalsViewComponent } from './goals/goals-view/goals-view.component';
+import { ProfileViewComponent } from './profile/profile-view/profile-view.component';
+import { QuestsViewComponent } from './quests/quests-view/quests-view.component';
+import { AchievementsViewComponent } from './achievements/achievements-view/achievements-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'goals',
+    component: GoalsViewComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileViewComponent
+  },
+  {
+    path: 'quests',
+    component: QuestsViewComponent
+  },
+  {
+    path: 'achievements',
+    component: AchievementsViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
