8273367b597fb1a2b9c988a5f042d496db01836d
asarav <asarav@umich.edu>
2019-02-24 19:22:58 -0500

    Start work for storage of goals data

--- src/app/goals/goals-view/goals-view.component.ts
+++ src/app/goals/goals-view/goals-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SaveNavService } from 'src/app/core/save-nav.service';
+const DataStore = require('nedb')
 
 @Component({
   selector: 'goals-view',
@@ -9,19 +10,53 @@ import { SaveNavService } from 'src/app/core/save-nav.service';
 })
 export class GoalsViewComponent implements OnInit {
   subscription:Subscription;
-  
+  userGoalsData;//Datastore
+  dataReady = false;
+  goalsData = '';//Text
+  data;//Actual row in database
+
   constructor(private saveNavService: SaveNavService) {
+    console.log(this);
+    var that = this;
+    this.userGoalsData = new DataStore({ filename: 'userGoalsData.data', autoload: true });
+    console.log(1);
+    console.log(this.userGoalsData);
+    this.userGoalsData.findOne({ _id: 'goals' }, function(err, data) {
+      console.log(data)
+      console.log(that);
+      if(data) {
+        that.data = data;
+        that.goalsData = data.data;
+      }
+      that.dataReady = true;
+    });
   }
 
   ngOnInit() {
     this.subscription = this.saveNavService.saveItem$
-       .subscribe(data => {
-         console.log(data);
+       .subscribe(subscriptionData => {
+         console.log(3);
+         console.log(subscriptionData);
+         if(this.dataReady) {
+           console.log(4);
+            this.saveData();
+         }
        });
   }
   ngOnDestroy() {
     // prevent memory leak when component is destroyed
+    this.saveData();
     this.subscription.unsubscribe();
+  }
+
+  saveData() {
+    if(this.dataReady) {
+      this.userGoalsData.update({ _id: 'goals'},
+      { _id: 'goals', data: this.goalsData },
+      { upsert: true }, function() {
+        console.log('Saved');
+      });
+    }
   }
 
 }
--- src/app/goals/goals.module.ts
+++ src/app/goals/goals.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoalsViewComponent } from './goals-view/goals-view.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [GoalsViewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class GoalsModule { }
