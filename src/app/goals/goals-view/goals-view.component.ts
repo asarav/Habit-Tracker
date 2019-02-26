0b65db8b29eb3f6f91952714895b755e0a627e95
asarav <asarav@umich.edu>
2019-02-25 20:15:46 -0500

    Bring in fs and electron-json-storage for alternate data handling methods

--- src/app/goals/goals-view/goals-view.component.ts
+++ src/app/goals/goals-view/goals-view.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SaveNavService } from 'src/app/core/save-nav.service';
-const DataStore = require('nedb')
 
 @Component({
   selector: 'goals-view',
@@ -10,37 +9,14 @@ const DataStore = require('nedb')
 })
 export class GoalsViewComponent implements OnInit {
   subscription:Subscription;
-  userGoalsData;//Datastore
-  dataReady = false;
   goalsData = '';//Text
-  data;//Actual row in database
 
   constructor(private saveNavService: SaveNavService) {
-    console.log(this);
-    var that = this;
-    this.userGoalsData = new DataStore({ filename: 'userGoalsData.data', autoload: true });
-    console.log(1);
-    console.log(this.userGoalsData);
-    this.userGoalsData.findOne({ _id: 'goals' }, function(err, data) {
-      console.log(data)
-      console.log(that);
-      if(data) {
-        that.data = data;
-        that.goalsData = data.data;
-      }
-      that.dataReady = true;
-    });
   }
 
   ngOnInit() {
     this.subscription = this.saveNavService.saveItem$
        .subscribe(subscriptionData => {
-         console.log(3);
-         console.log(subscriptionData);
-         if(this.dataReady) {
-           console.log(4);
-            this.saveData();
-         }
        });
   }
   ngOnDestroy() {
@@ -50,13 +26,6 @@ export class GoalsViewComponent implements OnInit {
   }
 
   saveData() {
-    if(this.dataReady) {
-      this.userGoalsData.update({ _id: 'goals'},
-      { _id: 'goals', data: this.goalsData },
-      { upsert: true }, function() {
-        console.log('Saved');
-      });
-    }
   }
 
 }
