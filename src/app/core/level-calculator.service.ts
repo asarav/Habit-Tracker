1659a59175701d437a15461764eaaa8cce589873
asarav <asarav@umich.edu>
2019-03-10 18:11:55 -0400

    Update level-calculator.service.ts

--- src/app/core/level-calculator.service.ts
+++ src/app/core/level-calculator.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DataStoreService } from './data-store.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,10 @@ export class LevelCalculatorService {
   maxLevel = 0;
   levelBreakdown = {};
 
-  constructor(goals) {
-    if(goals) {
-      this.calculateLevelData(goals);
+  constructor(private storage: DataStoreService) {
+    var goalData = storage.getItem('goals');
+    if(goalData && goalData.goals) {
+      this.calculateLevelData(goalData);
     }
   }
 
@@ -30,7 +32,13 @@ export class LevelCalculatorService {
       });
     });
 
-    var denominator = 3;
     //TODO: Calculate level breakdown to determine max number of levels.
+    //Levels will require more completed goals as levels increase. We plan on using a series of exponential figures starting at 3^1.8 if the number of goals is above 50.
+    //We will use an exponent of 1.5 for lower amounts.
+    //E.g., total goals = 3^1.8 + 4^1.8 + 5^1.8 + ...
+    //experience count 1 will use 3^1.8 and so on. Initially it will be easier to level up because the experience count requirement is lower.
+    //However, difficulty will rise as experience count increases.
+
+    //https://gamedev.stackexchange.com/questions/13638/algorithm-for-dynamically-calculating-a-level-based-on-experience-points
   }
 }
