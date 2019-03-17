f7a12502e1c9cd179c6a29d26e7dc4b889d81691
asarav <asarav@umich.edu>
2019-03-17 18:57:37 -0400

    Add ability to preview loaded image

--- src/app/profile/profile-view/profile-view.component.ts
+++ src/app/profile/profile-view/profile-view.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-view.component.less']
 })
 export class ProfileViewComponent implements OnInit {
+  private imageMessage;
+  private imagePath;
+  imgURL;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  imageUploaded(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.imageMessage = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
   }
 
 }
