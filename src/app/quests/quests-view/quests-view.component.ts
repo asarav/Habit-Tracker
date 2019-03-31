68ccd6e11c178f55f35532cb7f6e8fe6128c21c0
asarav <asarav@umich.edu>
2019-03-31 16:35:36 -0400

    Add list of guilds

--- src/app/quests/quests-view/quests-view.component.ts
+++ src/app/quests/quests-view/quests-view.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quests-view.component.less']
 })
 export class QuestsViewComponent implements OnInit {
+  guilds = [ { code: 'AD', name: 'Adventurers Guild' },
+    { code: 'AR', name: 'Artists Guild' },
+    { code: 'AS', name: 'Assassins Guild'},
+    { code: 'BA', name: 'Bards Guild' },
+    { code: 'GE', name: 'Gentlemens Guild' },
+    { code: 'MA', name: 'Mages Guild' },
+    { code: 'ME', name: 'Merchants Guild' }
+  ];
 
   constructor() { }
 
